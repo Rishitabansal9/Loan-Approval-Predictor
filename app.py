f7ca5e5e7c2210e962dc9a4a9f2cf3827aee6185
Rishita Bansal <rishitabansal2004@gmail.com>
2023-12-14 21:41:38 +0530

    Final

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
+from flask import jsonify
 
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
@@ -11,29 +12,25 @@ app=application
 
 ## Route for home page
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
 @app.route('/', methods=['GET', 'POST'])
 def predict_datapoint():
-    if request.method == 'GET':
-        return index
-    else:
+    if request.method == 'POST':
         data = CustomData(
-            income = request.form.get('income'),
-            existingLoans = request.form.get('existingLoans'),
-            loanAmount = request.form.get('loanAmount'),
-            loanTenure = request.form.get('loanTenure'),
-            occupation = request.form.get('occupation')
+            income=request.form.get('income'),
+            existingLoans=request.form.get('existingLoans'),
+            loanAmount=request.form.get('loanAmount'),
+            loanTenure=request.form.get('loanTenure'),
+            occupation=request.form.get('occupation')
         )
         pred_df = data.get_data_as_data_frame()
-        print(pred_df)  # Check the console/terminal for this print output
-
         predict_pipeline = PredictPipeline()
         results = predict_pipeline.predict(pred_df)
-        print(results[0])
-        return render_template('index.html', results=results[0])
+
+        # Return the result as JSON
+        return jsonify({'prediction_result': results[0]})
+
+    # If it's a GET request, return the template
+    return render_template('index.html')
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0", debug=True)
